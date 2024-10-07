9a2d552a907fb1f8650e23750fed0483e4d92235
Berke Kaya <b.kaya@naxxt.com>
2024-10-07 17:47:43 +0300

    optimized

--- Libs/NX.Libs.MemoryCacheLib/Interfaces/IMemoryCacheManager.cs
+++ Libs/NX.Libs.MemoryCacheLib/Interfaces/IMemoryCacheManager.cs
@@ -4,7 +4,8 @@
     {
         void Add(string key, TValue value);
         void AddRange(string key, params TValue[] values);
-        IEnumerable<TValue>? Get(string key);
+        IEnumerable<T>? GetAll<T>(string key) where T : TValue;
+        IEnumerable<T>? GetWithFilter<T>(string key, Func<T, bool> filter) where T : TValue;
         void Update<T>(string key, Func<T, bool> filter, Action<T> updateAction) where T : TValue;
         void Remove(string key, Func<TValue, bool> filter);
         IEnumerable<TValue>? SafeDelete(string key);
